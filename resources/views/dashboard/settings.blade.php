32377f65d33680e0d3847f3fd2b35513785f5700
RavLyr <ravlybrb@gmail.com>
2023-01-08 22:27:26 +0700

    menambahkan fitur ganti password

--- resources/views/dashboard/settings.blade.php
+++ resources/views/dashboard/settings.blade.php
@@ -55,6 +55,45 @@
             <button type="submit"
                 class="mt-8 h-12 text-white font-poppins font-semibold text-lg rounded-lg bg-mainblue w-full">Submit</button>
         </form>
+
+    </div>
+    <div class="ml-16 w-2/5 mb-3 font-poppins ">
+        <form method="post" action="{{ route('password.update') }}">
+            @csrf
+            @method('put')
+    
+            <div class="mb-6">
+                <x-input-label for="current_password" :value="__('Current Password')" class="mb-3"/>
+                <x-text-input id="current_password" name="current_password" type="password" class="mt-1 block w-full" autocomplete="current-password" />
+                <x-input-error :messages="$errors->updatePassword->get('current_password')" class="mt-2" />
+            </div>
+    
+            <div class="mb-6">
+                <x-input-label for="password" :value="__('New Password')" class="mb-3" />
+                <x-text-input id="password" name="password" type="password" class="mt-1 block w-full" autocomplete="new-password" />
+                <x-input-error :messages="$errors->updatePassword->get('password')" class="mt-2" />
+            </div>
+    
+            <div class="mb-6">
+                <x-input-label for="password_confirmation" :value="__('Confirm Password')" class="mb-3"/>
+                <x-text-input id="password_confirmation" name="password_confirmation" type="password" class="mt-1 block w-full" autocomplete="new-password" />
+                <x-input-error :messages="$errors->updatePassword->get('password_confirmation')" class="mt-2" />
+            </div>
+    
+            <div class="flex items-center gap-4">
+                <x-primary-button>{{ __('Save') }}</x-primary-button>
+    
+                @if (session('status') === 'password-updated')
+                    <p
+                        x-data="{ show: true }"
+                        x-show="show"
+                        x-transition
+                        x-init="setTimeout(() => show = false, 2000)"
+                        class="text-sm text-gray-600"
+                    >{{ __('Saved.') }}</p>
+                @endif
+            </div>
+        </form>
     </div>
 </div>
 @endsection
